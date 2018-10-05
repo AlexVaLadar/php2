4a410398c55ce9d908730c9c153f2786665f7f1f
AlexVaLadar <kljak.sa@mail.ru>
2018-10-05 14:26:55 +0300

    add head file blocks

---
+++ shop_new/blocks/head.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <title>Shop-Online</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css" media="all">
+    <script src="js/jquery.js"></script>
+    <script src="js/my.js"></script>
+</head>
